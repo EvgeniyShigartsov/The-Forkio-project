057cdcef2196e3aea64e230260582237af181329
Evgeniy Shigartsov <evgeniy.shigartsov2@gmail.com>
2020-11-12 15:52:10 +0200

    mobile header done

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+window.addEventListener('click', toggleBurger);
+function toggleBurger(event){
+    const burger= document.querySelector('.burger');
+    const navigation = document.querySelector('.navigation ');
+    const isParentExistNav = event.target.closest('.navigation');
+    const isParentExistBurger = event.target.closest('.burger');
+    
+    if (isParentExistBurger){
+        burger.classList.toggle('active');
+        navigation.classList.toggle('active');
+    } else if (burger.classList.contains('active') && navigation.classList.contains('active') && isParentExistBurger === null && isParentExistNav === null){
+        burger.classList.remove('active');
+        navigation.classList.remove('active');
+    }
+}
